28b7231cb684e7b9a8c0d3759a41d41506d4f24e
Biswajit Aich <0000biswajitaich@gmail.com>
2025-05-05 16:01:49 +0530

    db update

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from utils.getLLM import getLLM
 from utils.getChain import getChain
 from pydantic import BaseModel
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     retriever = load_vectorstore()
@@ -19,12 +20,15 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Portfolio Bot API", lifespan=lifespan)
 
+
 class ChatRequest(BaseModel):
     question: str
+
 
 @app.get("/")
 async def root():
     return {"message": "portfolio-bot is live!"}
+
 
 @app.post("/chat")
 async def chat(request: ChatRequest):
@@ -32,20 +36,55 @@ async def chat(request: ChatRequest):
         result = app.state.chain.invoke({"question": request.question})
         return {"answer": result}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error processing request: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Error processing request: {str(e)}"
+        )
+
 
 @app.get("/vector-store-rerun")
 async def vector_store_rerun():
     try:
-        create_vectorstore()
-        return {"message": "Vector store successfully created!"}
+        if os.getenv("APP_ENV", "") == "development":
+            create_vectorstore()
+            return {
+                "env": "development",
+                "message": "Vector store successfully created!",
+            }
+        else:
+            return {"env": "not development"}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error processing request: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Error processing request: {str(e)}"
+        )
+
+
+@app.get("/test")
+async def test():
+    try:
+        if os.getenv("APP_ENV", "") == "development":
+            retriever = load_vectorstore()
+            prompt_template = getPromptTemplate()
+            # data = await chain.ainvoke("what is biswajit's height?")
+            context = await retriever.ainvoke("what is biswajit's height?")
+            # data = await prompt_template.ainvoke({
+            #     "question": "what is biswajit's height?",
+            #     "context": context
+            # })
+            return {
+                "env": "development",
+                "data": context,
+            }
+        else:
+            return {"env": "not development"}
+    except Exception as e:
+        return {"error": str(e)}
+
 
 def main():
     print("Hello from portfolio-bot!")
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
 
 if __name__ == "__main__":
     main()
